113c24746bb4c3e1dc463e1f10bb39e4d02c102b
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2023-12-21 23:15:12 +0300

    Updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,53 +1,29 @@
 import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
-const Footer = ({ linkTextColors, copyrightTextColor }) => {
+const Footer = () => {
   return (
-    <>
-      <div className=" flex justify-between items-center p-4 h-[120px]">
-        {/* Proje İsmi  */}
-        <div className="flex items-center">
-          <h2 className="text-white text-3xl p-4 ">
-            Go <span className="font-bold text-white"> Food</span>
-          </h2>
-        </div>
-
-        {/* Yönlendirme Linkleri */}
-        <div
-          className={`text-white flex flex-col items-center space-y-2 sm:flex-row sm:space-y-0 sm:space-x-9`}
-        >
-          <button
-            className={`text-base hover:underline`}
-            style={{ color: linkTextColors }}
-          >
-            Destek
-          </button>
-          <button
-            className={`text-base hover:underline`}
-            style={{ color: linkTextColors }}
-          >
-            Gizlilik Politikası
-          </button>
-          <button
-            className={`text-base hover:underline`}
-            style={{ color: linkTextColors }}
-          >
-            Kullanım Koşulları
-          </button>
-        </div>
-
-        {/* Copyright */}
+    <footer className="bg-orange-500 p-4 mt-8">
+      <div className="container mx-auto flex justify-between items-center text-black font-semibold">
         <div>
-          <div className="flex items-center justify-center">
-            <p
-              className={`text-white text-base mr-40`}
-              style={{ color: copyrightTextColor }}
-            >
-              Bahar & Mehmet Akif
-            </p>
-          </div>
+          <p className="text-sm mb-2">&copy; Bahar & Mehmet Akif</p>
+        </div>
+        <div className="flex items-center">
+          <a className="text-white mr-2 hover:text-gray-300 cursor-pointer">
+            <FaFacebook className="h-5 w-5" />
+          </a>
+          <a className="text-white mr-2 hover:text-gray-300 cursor-pointer">
+            <FaTwitter className="h-5 w-5" />
+          </a>
+          <a className="text-white mr-2 hover:text-gray-300 cursor-pointer">
+            <FaInstagram className="h-5 w-5" />
+          </a>
+          <a className="text-white hover:text-gray-300 cursor-pointer">
+            <FaLinkedin className="h-5 w-5" />
+          </a>
         </div>
       </div>
-    </>
+    </footer>
   );
 };
 
